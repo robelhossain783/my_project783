e87467b0f58f43939c1cbe3d63df0899b31f64b2
Robel Hossain <robel.hossain@upaybd.com>
2026-01-15 13:38:03 +0600

    fix

--- my_project783/wsgi.py
+++ my_project783/wsgi.py
@@ -7,10 +7,22 @@ For more information on this file, see
 https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
 """
 
+# import os
+#
+# from django.core.wsgi import get_wsgi_application
+#
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'my_project783.settings')
+#
+# application = get_wsgi_application()
+
 import os
+import sys
+
+path = '/home/yourusername/my_project783'
+if path not in sys.path:
+    sys.path.append(path)
+
+os.environ['DJANGO_SETTINGS_MODULE'] = 'my_project783.settings'
 
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'my_project783.settings')
-
 application = get_wsgi_application()
